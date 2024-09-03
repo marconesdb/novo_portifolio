9e5b93a10d41e1db23516d7aad642d31d81a6c24
marconebrito <marcone.brito@elystech.com.br>
2024-09-02 23:16:04 -0300

    Finalização dos Cards

--- src/sections/Projects/Projects.styles.ts
+++ src/sections/Projects/Projects.styles.ts
@@ -6,7 +6,7 @@ export const Container = styled.section`
   align-items: center;
   justify-content: center;
   background-color: #1b1e23;
-  min-height: 100vh; /* Muda para min-height para permitir expansão */
+  min-height: 100vh;
   padding: 20px;
   position: relative;
   transform: translateY(-200px);
@@ -20,12 +20,16 @@ export const Title = styled.div`
   color: white;
   display: flex;
   align-items: center;
+
+  span {
+    color: #d9a400; /* Cor amarela */
+  }
 `;
 
 export const Line = styled.div`
   height: 2px;
   width: 600px;
-  background-color: yellow;
+  background-color: #d9a400;
   margin-left: 10px;
 `;
 
@@ -33,20 +37,85 @@ export const Cards = styled.div`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   gap: 20px;
-  margin-top: 50px; /* Ajuste conforme necessário */
+  margin-top: 50px;
 `;
 
 export const Card = styled.div`
   background-color: #1b1e23;
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
-  padding: 20px;
-  text-align: center;
-  font-size: 18px;
-  color: black;
+  color: white;
   width: 300px;
-  height: 400px; /* Ajuste conforme necessário */
+  height: 450px;
   display: flex;
+  flex-direction: column;
   align-items: center;
-  justify-content: center;
+  justify-content: space-between;
   border: solid 1px #fff;
+`;
+
+export const CardImage = styled.img`
+  width: 100%;
+  height: 200px;
+  object-fit: cover;
+`;
+
+export const CardTitle = styled.h3`
+  font-size: 24px;
+  font-weight: 500;
+  color: #fff;
+  text-align: center;
+  width: 300px;
+  padding: 10px;
+`;
+
+export const CardSubtitle = styled.h3`
+  font-size: 14px;
+  font-weight: 400;
+  color: #ABB2BF;
+  text-align: center;
+  width: 300px;
+  padding: 10px;
+  border: 1px solid #fff;
+`;
+
+export const CardText = styled.p`
+  font-size: 14px;
+  font-weight: 400;
+  color: #ABB2BF;
+  text-align: center;
+  flex: 1;
+  margin-bottom: 15px;
+`;
+
+export const CardButtons = styled.div`
+  display: flex;
+  gap: 20px;
+  padding: 15px;
+
+  a {
+    text-decoration: none;  /* Remove sublinhado do link */
+  }
+
+  button {
+    background-color: #1b1e23;
+    border: 1px solid #fff;
+    color: white;
+    padding: 10px 20px;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    gap: 10px;  /* Espaço entre o ícone e o texto */
+    transition: background-color 0.3s;
+
+    &:hover {
+      /* background-color: #d9a400; */
+      background-color: #1b1e23;
+      color: white;
+      border: solid #d9a400;
+    }
+
+    svg {
+      font-size: 18px;  /* Tamanho do ícone */
+    }
+  }
 `;
--- src/sections/Projects/Projects.tsx
+++ src/sections/Projects/Projects.tsx
@@ -1,27 +1,145 @@
-// components/Home/Home.tsx
 import React from 'react';
-import {Container, Title, Line, Cards, Card} from './Projects.styles'
+import { Container, Title, Line, Cards, Card, CardImage, CardTitle, CardText, CardButtons, CardSubtitle } from './Projects.styles';
+import { FaGithub, FaExternalLinkAlt } from 'react-icons/fa';  // Importação dos ícones
+
 const Projects: React.FC = () => {
   return (
     <Container>
       <Title>
-      <span>#projects</span>
-      <Line />
+        <span>#</span>
+        <p>projects</p>
+        <Line />
       </Title>
       <Cards>
-      <Card>Card 1</Card>
-      <Card>Card 2</Card>
-      <Card>Card 3</Card>
-      <Card>Card 4</Card>
-      <Card>Card 5</Card>
-      <Card>Card 6</Card>
+         {/* Card 1 */}
+        <Card>
+          <CardImage src="https://via.placeholder.com/300x200" alt="Imagem do Projeto 1" />
+          <CardSubtitle>HTML SCSS Python Flask</CardSubtitle>
+          <CardText>
+            <CardTitle>Nome do Projeto</CardTitle>
+            Este é o texto descritivo do projeto. Você pode adicionar mais detalhes aqui.
+          </CardText>
+          <CardButtons>
+            <a href="https://github.com/seu-usuario/seu-repositorio" target="_blank" rel="noopener noreferrer">
+              <button>
+                <FaGithub /> Github
+              </button>
+            </a>
+            <a href="https://seu-site.com" target="_blank" rel="noopener noreferrer">
+              <button>
+                <FaExternalLinkAlt /> Page
+              </button>
+            </a>
+          </CardButtons>
+        </Card>
+        {/* Card 2 */}
+        <Card>
+          <CardImage src="https://via.placeholder.com/300x200" alt="Imagem do Projeto 1" />
+          <CardSubtitle>HTML SCSS Python Flask</CardSubtitle>
+          <CardText>
+            <CardTitle>Nome do Projeto</CardTitle>
+            Este é o texto descritivo do projeto. Você pode adicionar mais detalhes aqui.
+          </CardText>
+          <CardButtons>
+            <a href="https://github.com/seu-usuario/seu-repositorio" target="_blank" rel="noopener noreferrer">
+              <button>
+                <FaGithub /> Github
+              </button>
+            </a>
+            <a href="https://seu-site.com" target="_blank" rel="noopener noreferrer">
+              <button>
+                <FaExternalLinkAlt /> Page
+              </button>
+            </a>
+          </CardButtons>
+        </Card>
+         {/* Card 3 */}
+         <Card>
+          <CardImage src="https://via.placeholder.com/300x200" alt="Imagem do Projeto 1" />
+          <CardSubtitle>HTML SCSS Python Flask</CardSubtitle>
+          <CardText>
+            <CardTitle>Nome do Projeto</CardTitle>
+            Este é o texto descritivo do projeto. Você pode adicionar mais detalhes aqui.
+          </CardText>
+          <CardButtons>
+            <a href="https://github.com/seu-usuario/seu-repositorio" target="_blank" rel="noopener noreferrer">
+              <button>
+                <FaGithub /> Github
+              </button>
+            </a>
+            <a href="https://seu-site.com" target="_blank" rel="noopener noreferrer">
+              <button>
+                <FaExternalLinkAlt /> Page
+              </button>
+            </a>
+          </CardButtons>
+        </Card>
+        {/* Card 4 */}
+        <Card>
+          <CardImage src="https://via.placeholder.com/300x200" alt="Imagem do Projeto 1" />
+          <CardSubtitle>HTML SCSS Python Flask</CardSubtitle>
+          <CardText>
+            <CardTitle>Nome do Projeto</CardTitle>
+            Este é o texto descritivo do projeto. Você pode adicionar mais detalhes aqui.
+          </CardText>
+          <CardButtons>
+            <a href="https://github.com/seu-usuario/seu-repositorio" target="_blank" rel="noopener noreferrer">
+              <button>
+                <FaGithub /> Github
+              </button>
+            </a>
+            <a href="https://seu-site.com" target="_blank" rel="noopener noreferrer">
+              <button>
+                <FaExternalLinkAlt /> Page
+              </button>
+            </a>
+          </CardButtons>
+        </Card>
+        {/* Card 5 */}
+        <Card>
+          <CardImage src="https://via.placeholder.com/300x200" alt="Imagem do Projeto 1" />
+          <CardSubtitle>HTML SCSS Python Flask</CardSubtitle>
+          <CardText>
+            <CardTitle>Nome do Projeto</CardTitle>
+            Este é o texto descritivo do projeto. Você pode adicionar mais detalhes aqui.
+          </CardText>
+          <CardButtons>
+            <a href="https://github.com/seu-usuario/seu-repositorio" target="_blank" rel="noopener noreferrer">
+              <button>
+                <FaGithub /> Github
+              </button>
+            </a>
+            <a href="https://seu-site.com" target="_blank" rel="noopener noreferrer">
+              <button>
+                <FaExternalLinkAlt /> Page
+              </button>
+            </a>
+          </CardButtons>
+        </Card>
+         {/* Card 6 */}
+         <Card>
+          <CardImage src="https://via.placeholder.com/300x200" alt="Imagem do Projeto 1" />
+          <CardSubtitle>HTML SCSS Python Flask</CardSubtitle>
+          <CardText>
+            <CardTitle>Nome do Projeto</CardTitle>
+            Este é o texto descritivo do projeto. Você pode adicionar mais detalhes aqui.
+          </CardText>
+          <CardButtons>
+            <a href="https://github.com/seu-usuario/seu-repositorio" target="_blank" rel="noopener noreferrer">
+              <button>
+                <FaGithub /> Github
+              </button>
+            </a>
+            <a href="https://seu-site.com" target="_blank" rel="noopener noreferrer">
+              <button>
+                <FaExternalLinkAlt /> Page
+              </button>
+            </a>
+          </CardButtons>
+        </Card>
       </Cards>
     </Container>
   );
 };
 
 export default Projects;
-
-
-
-
